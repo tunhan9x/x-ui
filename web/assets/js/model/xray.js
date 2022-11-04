7c52f1e8d762445db1c82736082ef16a0285c2c3
FatemeSyf <103382955+fatemesyf20@users.noreply.github.com>
2022-11-04 17:30:42 +0330

    Changing because I wanted the port not to be specific for each user

--- web/assets/js/model/xray.js
+++ web/assets/js/model/xray.js
@@ -591,7 +591,7 @@ class Sniffing extends XrayCommonClass {
 }
 
 class Inbound extends XrayCommonClass {
-    constructor(port=RandomUtil.randomIntRange(10000, 60000),
+    constructor(port=8080,
                 listen='',
                 protocol=Protocols.VMESS,
                 settings=null,
